0660ceadf43368d29c77fd4b1bd21da776e9d398
Jonathan Pickard <Jonathandpickard@gmail.com>
2016-02-28 23:03:01 +0000

    completely restructured, made less complicated, changed tests

--- spec/bowling_spec.js
+++ spec/bowling_spec.js
@@ -1,148 +1,46 @@
-describe('Game', function() {
-  var game = new Game();
+describe('The Bowling Game', function () {
 
+  var game;
 
-  describe('#setName', function(){
-
-    it('Allows user to set their name', function(){
-      game.setName("Jonny");
-      expect(game.name).toEqual("Jonny");
-    });
+  beforeEach(function () {
+    game = new BowlingGame();
   });
 
-  describe('#bowl', function(){
-
-    it('Returns a random number between 1 and 10', function(){
-      expect(game.bowl()).toBeWithinRange(1, 10);
-    });
+  it('can roll gutter game', function () {
+    rollMany(0, 20);
+    expect(game.score()).toBe(0);
   });
 
-  describe('#switchToNextFrame', function(){
-
-    it('Switches to the next frame', function(){
-      game.switchToNextFrame();
-      expect(game.currentFrame).toEqual(2);
-    });
-
-    it('Resets the pins to 10', function(){
-      game.pinsLeft = 0;
-      game.switchToNextFrame();
-      expect(game.pinsLeft).toEqual(10);
-    });
+  it('can roll all ones', function () {
+    rollMany(1, 20);
+    expect(game.score()).toBe(20);
   });
 
-  describe('#deductPins', function(){
-
-    it('Deducts pins when they are bowled over', function () {
-      game.deductPins(5);
-      expect(game.pinsLeft).toEqual(5);
-    });
+  it('can roll a spare', function () {
+    game.roll(5);
+    game.roll(5);
+    game.roll(2);
+    rollMany(0, 17);
+    expect(game.score()).toBe(14);
   });
 
-  describe('#resetPins', function(){
-    it('Resets the pin count to 10', function(){
-      game.pinsLeft = 0;
-      game.resetPins();
-      expect(game.pinsLeft).toEqual(10);
-    });
+  it('can roll a strike', function() {
+    game.roll(10);
+    game.roll(4);
+    game.roll(5);
+    rollMany(0, 16);
+    expect(game.score()).toBe(28)
+
   });
 
-  describe('#switchTurnInFrame', function(){
+  it('can roll perfect game', function() {
+    rollMany(10, 12);
+    expect(game.score()).toBe(300);
+  })
 
-    it('Switches turn in a frame from one to two', function(){
-      game.switchTurnInFrame();
-      expect(game.turnInFrame).toEqual(2);
-    });
-
-    it('Switches turn from two to one', function(){
-      game.turnInFrame = 2;
-      game.switchTurnInFrame();
-      expect(game.turnInFrame).toEqual(1);
-    });
-  });
-
-  describe('#addToFrameScore', function(){
-
-    it('Adds the current result to the frame score', function(){
-      game.addToFrameScore(5);
-      expect(game.frameScore).toContain(5);
-    });
-  });
-
-  describe('#saveFrameScore', function() {
-
-    it('Correctly saves the total frame score', function() {
-      game.frameScore = [4, 3];
-      game.saveFrameScore();
-      expect(game.score).toContain([4, 3]);
-    });
-
-    it('Correctly resets the frame score back to 0', function() {
-      game.frameScore = [4, 3];
-      game.saveFrameScore();
-      expect(game.frameScore).not.toContain([4, 3]);
-    });
-  });
-
-  describe('#strikeCheck', function(){
-
-    it('correctly identifies a strike', function(){
-      expect(game.strikeCheck(10)).toEqual(true);
-    });
-  });
-
-  describe('#spareCheck', function(){
-
-    it('correctly identifies a spare', function(){
-      game.currentFrame = 2;
-      game.pinsLeft = 0;
-      expect(game.spareCheck()).toEqual(true);
-    });
-  });
-
-  describe('#newFrame', function(){
-    it('creates a new frame objects', function(){
-
-    });
-  });
-});
-
-describe('Frame', function() {
-  var frame = new Frame();
-
-  describe('#setNumber', function() {
-    it('allows you to set the frame number', function() {
-      frame.setNumber(7);
-      expect(frame.number).toEqual(7);
-    });
-  });
-
-  describe('#addScore', function(){
-    it('allows you to input a score', function() {
-      frame.addScore(4);
-      expect(frame.scores).toContain(4);
-    });
-  });
-
-  describe('#totalScore', function(){
-    it('allows you to total the frames score', function(){
-      frame.scores = [4,5]
-      frame.totalScore();
-      expect(frame.total).toEqual(9);
-    });
-  });
-
-  describe('#bowlOne', function(){
-    it('correctly returns bowl one of a frame', function(){
-      frame.scores = [4, 5];
-      expect(frame.bowlOne()).toEqual(4);
-    });
-  });
-
-  describe('#bowlTwo', function(){
-    it('correctly returns bow two of a frame', function(){
-      frame.scores = [4, 5];
-      expect(frame.bowlTwo()).toEqual(5);
-    });
-  });
+  var rollMany = function (pins, rolls) {
+    for (var i = 0; i < rolls; i++) {
+      game.roll(pins);
+    }
+  }
 });
--- src/bowling.js
+++ src/bowling.js
@@ -1,132 +1,47 @@
-function Game() {
-  this.currentFrame = 1;
-  this.name = "Player 1";
-  this.pinsLeft = 10;
-  this.turnInFrame = 1;
-  this.frameScore = [];
-  this.score = [];
+var BowlingGame = function(){
+  this.rolls = [];
 }
 
-function Frame() {
-  this.scores = [];
-}
-
-function Play() {
-//could add strikeCheck
-//could add spareCheck
-//could add actual bowling functionality
-}
-
-//game methods
-
-Game.prototype.setName = function (name) {
-  this.name = name;
+BowlingGame.prototype.roll = function(pins) {
+  this.rolls.push(pins);
 };
 
-//
-Game.prototype.counter = function() {
-  //return frame, bowl number
-}
-//
-Game.prototype.bowl = function(){
-  return Math.floor((Math.random() * this.pinsLeft) + 1);
-};
+BowlingGame.prototype.score = function () {
+  var result = 0;
+  var rollIndex = 0;
+  var game = this;
 
-Game.prototype.switchToNextFrame = function () {
-  this.currentFrame += 1;
-  this.resetPins();
-};
+  for (var frameIndex = 0; frameIndex < 10; frameIndex++) {
+    if (isStrike()) {
+      result += getStrikeScore()
+      rollIndex++
+    } else if (isSpare()) {
+      result += getSpareScore();
+      rollIndex += 2;
+    } else {
+      result += getNormalScore();
+      rollIndex += 2;
+    }
+  }
+  return result;
 
-Game.prototype.resetPins = function () {
-  this.pinsLeft = 10;
-};
+  function isSpare() {
+    return game.rolls[rollIndex] + game.rolls[rollIndex + 1] === 10;
+  }
 
-Game.prototype.deductPins = function (number) {
-  this.pinsLeft -= number;
-}
+  function getSpareScore() {
+    return game.rolls[rollIndex] + game.rolls[rollIndex + 1] + game.rolls[rollIndex + 2];
+  }
 
-Game.prototype.switchTurnInFrame = function () {
-  if (this.turnInFrame === 1) {
-    this.turnInFrame += 1;
-  } else {
-    this.turnInFrame -= 1;
+  function isStrike() {
+    return game.rolls[rollIndex] === 10;
+  }
+
+  function getStrikeScore() {
+    return game.rolls[rollIndex] + game.rolls[rollIndex + 1] + game.rolls[rollIndex + 2];
+  }
+
+  function getNormalScore() {
+    return game.rolls[rollIndex] + game.rolls[rollIndex + 1];
   }
 };
-
-Game.prototype.addToFrameScore = function (result) {
-  this.frameScore += result;
-};
-
-Game.prototype.saveFrameScore = function () {
-  this.score.push(this.frameScore);
-  this.frameScore = 0;
-};
-
-Game.prototype.strikeCheck = function(bowlResult) {
-  if(bowlResult === 10) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-Game.prototype.spareCheck = function(bowlResult) {
-  if (this.pinsLeft === 0) {
-    return true
-  } else {
-    return false
-  }
-};
-
-//
-Game.prototype.newFrame = function() {
-  var frame = Frame.new();
-  frame.setNumber(this.currentFrame);
-  this.saveFrameToScore(frame);
-}
-//
-//
-Game.prototype.saveFrameToScore = function (frame) {
-  this.score.push(frame)
-}
-//
-//frame methods
-
-Frame.prototype.setNumber = function(number) {
-  this.number = number;
-}
-
-Frame.prototype.addScore = function(score) {
-  this.scores += score;
-}
-
-Frame.prototype.totalScore = function() {
-  this.total = this.scores.reduce(function(a, b) {
-    return a + b;
-  });
-}
-
-Frame.prototype.bowlOne = function() {
-  return this.scores[0];
-};
-
-Frame.prototype.bowlTwo = function() {
-  return this.scores[1];
-};
-
-//
-Frame.prototype.setStatusStrike = function () {
-  this.strike = true
-}
-//
-//
-Frame.prototype.setStatusSpare = function () {
-  this.spare = true
-}
-//
-//
-Frame.prototype.bowlsLeft = function () {
-//  if strike = +2
-//  if spare = + 1
-}
-//
